37ca589b4ccf92b1f991f61d8c9ec57ada417ffc
Hrithik-22 <kedarehrithik@gmail.com>
2026-08-03 20:37:06 +0530

    story card done

--- src/screen-groups/StoryListUI.tsx
+++ src/screen-groups/StoryListUI.tsx
@@ -1,11 +1,58 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { BACKGROUND_COLOR } from "../constants/constant";
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+import {
+  StoryCard,
+  StoryListItemHeight,
+  StoryListItemWidth,
+  WindowWidth,
+} from "../components/StoryCard";
+import { BACKGROUND_COLOR, Stories } from "../constants/constant";
 
 const StoryListUI = () => {
+  const scrollOffset = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollOffset.value = event.contentOffset.x;
+    },
+  });
+  const ListPadding = WindowWidth - StoryListItemWidth;
+
   return (
     <View style={styles.container}>
-      <Text>StoryListUI</Text>
+      <View
+        style={{
+          height: StoryListItemHeight,
+          width: "100%",
+        }}
+      >
+        <Animated.ScrollView
+          scrollEventThrottle={16}
+          horizontal
+          onScroll={scrollHandler}
+          snapToInterval={StoryListItemWidth}
+          decelerationRate={"fast"}
+          disableIntervalMomentum
+          showsHorizontalScrollIndicator={true}
+          contentContainerStyle={{
+            width: StoryListItemWidth * Stories.length + ListPadding,
+          }}
+        >
+          {Stories.map((story, index) => {
+            return (
+              <StoryCard
+                scrollOffset={scrollOffset}
+                key={index}
+                index={index}
+                imageSource={story.image}
+              />
+            );
+          })}
+        </Animated.ScrollView>
+      </View>
     </View>
   );
 };
