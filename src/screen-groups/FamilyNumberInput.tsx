341e9cb2132de253318f9fc6d5fabf91825f8641
Hrithik-22 <kedarehrithik@gmail.com>
2026-08-24 21:59:41 +0530

    for now

--- src/screen-groups/FamilyNumberInput.tsx
+++ src/screen-groups/FamilyNumberInput.tsx
@@ -3,12 +3,18 @@ import { StyleSheet, Text, View } from "react-native";
 
 const FamilyNumberInput = () => {
   return (
-    <View>
-      <Text>FamilyNumberInput</Text>
+    <View style={styles.container}>
+      <Text> HIII</Text>
     </View>
   );
 };
 
 export default FamilyNumberInput;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
